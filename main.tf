95002636973aaceb40a6614d7146ff41b2e0072d
AndresGarcia15 <teherana@utb.edu.co>
2024-09-16 00:50:47 -0500

    Version 1.0

--- main.tf
+++ main.tf
@@ -1,52 +1,87 @@
-
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.67.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.67.0"
     }
-    required_version = ">= 1.2.0"
+  }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "private_key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "${path.module}/keys/private_key.pem"
+}
+
+resource "local_sensitive_file" "public_key" {
+  content  = tls_private_key.example.public_key_pem
+  filename = "${path.module}/keys/public_key.pub"
+}
+
+
+
+resource "aws_key_pair" "example" {
+  key_name   = "blockstellart1"
+  public_key = tls_private_key.example.public_key_openssh
 }
 
 resource "aws_security_group" "web" {
-    name        = "blockstellart_sg"
-    description = "Security gruop for web server"
-    
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        }
-    
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        }
+  name        = "blockstellart_sg"
+  description = "Security group for web server"
 
-    egress {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "blockstellart" {
-    ami           = "ami-0182f373e66f89c85"
-    instance_type = "t2.micro"
-    key_name      = "blockstellart"
-    security_groups = [aws_security_group.web.name]
-    tags = {
-        Name = "BlockstellartInstance"
-    }
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  security_groups = [aws_security_group.web.name]
+  tags = {
+    Name = "BlockstellartInstance"
+  }
 }
